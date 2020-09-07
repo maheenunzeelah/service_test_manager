e0fd9e9ccce95aa1f40d417b2272ae4d8805af95
Maheen Unzeelah <46377344+maheenunzeelah@users.noreply.github.com>
2020-09-08 00:22:22 +0500

    deploy

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 module.exports = {
 
-    url://'mongodb+srv://catechism_2019:FinalYear2019@cluster0-yqhv7.mongodb.net/test?authSource=admin&retryWrites=true&w=majority',
-    'mongodb://localhost/project1',
+    url:'mongodb+srv://catechism_2019:FinalYear2019@cluster0-yqhv7.mongodb.net/test?authSource=admin&retryWrites=true&w=majority',
+    // 'mongodb://localhost/project1',
     
     
     serverport: 3001 
